6991537d9b9dd20f4d23aff00296c1ceb0846eb8
Ben Riddle <benriddle10@hotmail.com>
2019-06-15 00:12:29 -0700

    Upload to New Repo
    Was working on the wrong repo.

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Ben Riddle
-
-## Sources
-* [Normalize CSS](https://necolas.github.io/normalize.css)
-* [jQuery](https://jquery.com)
-
+# web-2-final
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log('ok');
+$('h1').lettering();
+
+$('.gallery').Chocolat();
+
+$('.tabs').tabslet({
+    autorotate: true,
+    delay: 5000,
+    mouseevent: 'click',
+    attribute: 'href',
+    animation: true
+
+  });
